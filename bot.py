39faf32738f12e036bd269922f7c74bc50a03538
shermatovdoniyor99-design <shermatovdoniyor99@gmail.com>
2026-03-14 07:44:12 +0500

    Update bot.py
    5

--- bot.py
+++ bot.py
@@ -27,7 +27,9 @@ def get_sales():
     }
 
     r = requests.get(url, headers=headers, params=params)
-
+    print(r.url)
+print(r.status_code)
+print(r.text)
     if r.status_code == 200:
         data = r.json()
 
@@ -38,9 +40,7 @@ def get_sales():
 
     else:
         return f"❌ API xato: {r.status_code}\n{r.text}"
-print(r.url)
-print(r.status_code)
-print(r.text)
+
 def check_updates():
     global last_update
 
