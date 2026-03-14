42099b1895e121012a210ce2f12f1cdeb1548ba7
shermatovdoniyor99-design <shermatovdoniyor99@gmail.com>
2026-03-14 09:39:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,10 @@ def get_orders():
 
     url = "https://api-seller.uzum.uz/api/seller-openapi/v2/fbs/orders"
 
-    headers = {
-        "Authorization": UZUM_API_KEY
-    }
+ headers = {
+    "Authorization": f"Bearer {UZUM_API_KEY}",
+    "Accept": "application/json"
+}
 
     params = {
         "shopIds": SHOP_ID,
