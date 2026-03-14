39ac51ef9fcee5c8ab3e90039700053ffcb39343
shermatovdoniyor99-design <shermatovdoniyor99@gmail.com>
2026-03-14 10:35:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ UZUM_API_KEY = os.getenv("UZUM_API_KEY")
 
 SHOP_ID = [54161, 64857]
 
+last_orders = 0
+last_revenue = 0
+
 
 def send(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -39,7 +42,7 @@ def get_orders():
     return data.get("payload", {}).get("orders", [])
 
 
-def get_report():
+def get_stats():
 
     orders = get_orders()
 
@@ -50,12 +53,7 @@ def get_report():
         for item in o["items"]:
             revenue += item["price"] * item["quantity"]
 
-    return f"""
-📊 Uzum sotuv hisoboti
-
-📦 Buyurtmalar: {total_orders}
-💰 Tushum: {revenue} so'm
-"""
+    return total_orders, revenue
 
 
 send("🚀 Uzum analytics bot ishga tushdi")
@@ -63,8 +61,26 @@ send("🚀 Uzum analytics bot ishga tushdi")
 while True:
 
     try:
-        report = get_report()
+
+        total_orders, revenue = get_stats()
+
+        new_orders = total_orders - last_orders
+        new_revenue = revenue - last_revenue
+
+        report = f"""
+📊 Soatlik Uzum hisoboti
+
+🆕 Yangi buyurtmalar: {new_orders}
+💰 Yangi tushum: {new_revenue} so'm
+
+📦 Jami buyurtmalar: {total_orders}
+💵 Jami tushum: {revenue} so'm
+"""
+
         send(report)
+
+        last_orders = total_orders
+        last_revenue = revenue
 
     except Exception as e:
         print(e)
