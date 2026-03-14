8de185324ef073828f8bef5058329efac631bce6
shermatovdoniyor99-design <shermatovdoniyor99@gmail.com>
2026-03-14 09:24:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,17 @@
 import requests
 
-UZUM_API_KEY = "API_KEYINGIZ"
+url = "https://api-seller.uzum.uz/api/seller-openapi/v2/fbs/orders"
 
-def get_report():
+headers = {
+    "Authorization": "API_KEY"
+}
 
-    url = "https://api-seller.uzum.uz/api/seller-openapi/v2/fbs/orders"
+params = {
+    "shopIds": 54161,
+    "page": 0,
+    "size": 20
+}
 
-    headers = {
-        "Authorization": f"Bearer {UZUM_API_KEY}"
-    }
+r = requests.get(url, headers=headers, params=params)
 
-    r = requests.get(url, headers=headers)
-
-    data = r.json()
-
-    orders = data["content"]
-
-    total_orders = len(orders)
-    revenue = 0
-
-    for o in orders:
-        price = o["price"]
-        qty = o["quantity"]
-
-        revenue += price * qty
-
-    return f"""
-📊 Bugungi statistika
-
-Buyurtmalar: {total_orders}
-Tushum: {revenue} so'm
-"""
-
-print(get_report())
+print(r.json())
