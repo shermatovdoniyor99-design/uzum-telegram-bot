7384be113aa77ad56090a3afcf0165669e8ecb4a
shermatovdoniyor99-design <shermatovdoniyor99@gmail.com>
2026-03-14 05:49:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,31 @@ import time
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+UZUM_API_KEY = os.getenv("UZUM_API_KEY")
 
-def send_message(text):
+def send(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": text
-    }
-    requests.post(url, data=data)
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
 
-send_message("✅ Uzum bot serverda ishga tushdi!")
+def get_sales():
+    url = "https://api.uzum.uz/seller-api/v1/orders"
+    
+    headers = {
+        "Authorization": UZUM_API_KEY
+    }
+
+    r = requests.get(url, headers=headers)
+    
+    if r.status_code == 200:
+        data = r.json()
+        count = len(data)
+        return f"📦 Bugungi buyurtmalar: {count}"
+    else:
+        return "❌ Uzum API dan ma'lumot olinmadi"
+
+send("🚀 Uzum analytics bot ishga tushdi!")
 
 while True:
-    send_message("📊 Uzum bot ishlayapti")
+    report = get_sales()
+    send(report)
     time.sleep(3600)
